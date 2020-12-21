7bc11379adb653f2dfa90d8d22488d3cd3d4a64e
phazanic <76036798+phazanic@users.noreply.github.com>
2020-12-21 22:20:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile</title>
+    <style >
+        body{
+           text-align: center ;
+           
+        }
+        
+    </style>
+</head>
+<font size ="5">
+<body background="12.jpg" >
+<font color ="white">  
+    <h1>Profile</h1>
+    <img src="1234.jpg" width="12%"> 
+    <p>ชื่อ-นามสกุล:นายธีรเดช พาทัน
+    <p>ชื่อเล่น:มาร์ค
+    <p>วันเกิด: 23 มกร่คม 2544
+    <p>อายุ:19 ปี 
+    <p>งานอดิเรก:ดูหนัง ฟังเพลง
+    <p>สาขา:วิยาการข้อมูล 
+    <p>ติดต่อ:0955813395
+    <p>Email:aomsin.4480@gmail.com </p>
+</font>
+</body>
+
+</html>
